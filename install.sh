0ac6447130e9188a36ff1523f7cf9fd1ae460dbb
abc8806191 <49439251+abc8806191@users.noreply.github.com>
2023-07-10 21:11:07 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,32 @@
+echo "====================="
+echo "pip3套件開始安裝"
+echo "====================="
+pip3 install rsa
+pip3 install thrift==0.11.0
+pip3 install requests
+pip3 install pytz
+pip3 install bs4
+pip3 install googletrans==4.0.0-rc1
+pip3 install youtube_dl
+pip3 install yt_dlp
+pip3 install pafy
+pip3 install humanfriendly
+pip3 install asyncio
+pip3 install humanize
+pip3 install paramiko
+pip3 install hyper
+pip3 install twder
+pip3 install html5lib
+pip3 install httplib2
+pip3 install null
+pip3 install python-axolotl-curve25519
+pip3 install requests_futures
+pip3 install pycryptodome
+pip3 install pandas
+pip3 install qrcode
+pip3 install image
+pip3 install matplotlib
+
+echo "====================="
+echo "pip3套件 安裝完成"
+echo "====================="
