5d17d3cd3672a114d0bcc185a52727c1d4c62eeb
abc8806191 <49439251+abc8806191@users.noreply.github.com>
2024-05-03 03:39:24 +0800

    Update xin.sh

--- xin.sh
+++ xin.sh
@@ -3,8 +3,6 @@ apt update -y
 echo "APT資源更新成功"
 echo "開始安裝Python"
 apt install python3 -y
-sudo apt install pipx
-pipx ensurepath
 apt install python3-pip -y
 echo "python檔案安裝成功"
 apt install speedtest-cli -y
@@ -17,31 +15,31 @@ echo "====================="
 echo "====================="
 echo "pip3套件開始安裝"
 echo "====================="
-pipx install rsa
-pipx install thrift
-pipx install requests
-pipx install pytz
-pipx install beautifulsoup4
-pipx install googletrans
-pipx install youtube-dl
-pipx install yt-dlp
-pipx install pafy
-pipx install humanfriendly
-pipx install asyncio
-pipx install humanize
-pipx install paramiko
-pipx install hyper
-pipx install twder
-pipx install html5lib
-pipx install httplib2
-pipx install null
-pipx install python-axolotl-curve25519
-pipx install requests-futures
-pipx install pycryptodome
-pipx install pandas
-pipx install qrcode
-pipx install pillow
-pipx install matplotlib
+pip3 install rsa
+pip3 install thrift
+pip3 install requests
+pip3 install pytz
+pip3 install beautifulsoup4
+pip3 install googletrans
+pip3 install youtube-dl
+pip3 install yt-dlp
+pip3 install pafy
+pip3 install humanfriendly
+pip3 install asyncio
+pip3 install humanize
+pip3 install paramiko
+pip3 install hyper
+pip3 install twder
+pip3 install html5lib
+pip3 install httplib2
+pip3 install null
+pip3 install python-axolotl-curve25519
+pip3 install requests-futures
+pip3 install pycryptodome
+pip3 install pandas
+pip3 install qrcode
+pip3 install pillow
+pip3 install matplotlib
 
 echo "====================="
 echo "pip3套件 安裝完成"
